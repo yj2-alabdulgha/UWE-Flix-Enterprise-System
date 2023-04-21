6f4b51d9490659294688687f7833cc0a189da1b2
rao20Anushka <anushka20rao@gmail.com>
2023-04-21 14:12:16 +0100

    AR - Updated ticket models

--- UWEFLIXGROUP/crud/models.py
+++ UWEFLIXGROUP/crud/models.py
@@ -74,7 +74,10 @@ class FilmShowings(models.Model):
    ticket_quantity = models.IntegerField(default=150)
 
 class CustomerTickets(models.Model):
+    ticketid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     item = models.CharField(max_length=150)
     ticketprice = models.DecimalField(max_digits= 5)
     ticketquantity = models.PositiveIntegerField()
+    filmtitle = models.CharField(max_length=100)
+    filmduration = models.IntegerField(default=0)
 
